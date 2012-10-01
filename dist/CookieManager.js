40c176db64c24942bf4f54e531323a0ee5ad5bb9
th3mon <p.szelenberger@gmail.com>
2012-10-01 17:28:52 +0200

    +grunt.js config file and main code

---
+++ dist/CookieManager.js
@@ -0,0 +1,157 @@
+/*! CookieManager - v0.1.0 - 2012-10-01
+* https://github.com/th3mon/CookieManager
+* Copyright (c) 2012 Przemysław Szelenberger; Licensed MIT */
+
+(function(g, d, u, $) {
+    var CookieManager = function() {};
+
+    CookieManager.prototype.Utils = g.gazeta_pl.mobi.Utils;
+
+    CookieManager.init = function() {
+        return new this();
+    };
+
+    CookieManager.prototype.set = function() {
+        var
+            self = this,
+            args = arguments,
+            cookieData,
+            config,
+            name,
+            value,
+            expireDays,
+            path,
+
+            init = function() {
+                var t = {};
+
+                if ("string" === typeof args[0]) {
+                    name = args[0];
+                    value = args[1];
+
+                } else {
+                    config = args[0];
+
+                    name = config.name;
+                    value = config.value;
+                    expireDays = config.expireDays;
+                    path = config.path;
+                }
+
+                if ("object" === typeof value) {
+                    if (self.Utils.isBrowserSupportsJSON()) {
+                        value = self.Utils.escapeKeysInObj(value);
+                        value = JSON.stringify(value);
+                    } else {
+                        throw {
+                            message: "Your browser don't support JSON :-("
+                        };
+                    }
+                } else {
+                    value = encodeURIComponent(value);
+                }
+
+            },
+
+            prepareExpiryDate = function () {
+                if (expireDays) {
+                    var expireDate = new Date();
+                    expireDate.setDate(expireDate.getDate() + expireDays);
+
+                    return expireDate.toUTCString();
+                }
+            },
+
+            execute = function() {
+                cookieData = name + "=" + value;
+
+                if (expireDays) {
+                    cookieData += ";expires=" + prepareExpiryDate();
+                }
+
+                if (path) {
+                    cookieData += "; path=" + path;
+                }
+
+                document.cookie = cookieData;
+            };
+
+        init();
+        execute();
+    };
+
+    CookieManager.prototype.get = function(name) {
+        var
+            self = this,
+            nameEQ = name + "=",
+            splittedCookie = document.cookie.split(";"),
+            value = "",
+            parsed = {},
+            ret = null,
+            i,
+            singleCookie,
+
+            trimLeft = function(str) {
+                while (str.charAt(0) === ' '){
+                    str = str.substring(1, str.length);
+                }
+
+                return str;
+            };
+
+        i = splittedCookie.length;
+        // TODO: sproboj zastosowac for'a
+        do {
+            i--;
+
+            if (-1 === i) {
+                return;
+            }
+
+            singleCookie = trimLeft(splittedCookie[i]);
+
+            if (singleCookie.indexOf(nameEQ) === 0) {
+                value = singleCookie.substring(nameEQ.length, singleCookie.length);
+
+                if(-1 !== value.indexOf("{")) {
+                    while("object" !== typeof value) {
+                        value = JSON.parse(value);
+                    }
+
+                    ret = self.Utils.unescapeKeysInObj(value);
+                } else if (value === "undefined") {
+                    ret = undefined;
+                } else {
+                    ret = decodeURIComponent(value);
+                }
+            }
+
+        } while ((null === ret) && i);
+
+        return ret;
+    };
+
+    CookieManager.prototype.remove = function(conf) {
+        var self = this;
+
+        if ("string" === typeof conf) {
+            self.set({
+                name: conf,
+                expireDays: -1
+            });
+        } else {
+            self.set({
+                name: conf.name,
+                value: conf.value,
+                expireDays: -1,
+                path: conf.path
+            });
+        }
+    };
+
+    CookieManager.prototype.getNewInstance = function() {
+        return new this.constructor();
+    };
+
+    g.CookieManager = CookieManager;
+}(window, document, undefined, jQuery));
